95f5e0087061e00f103885a781faa473bf78665e
kwakdoh <144428446+kwakdoh@users.noreply.github.com>
2023-10-06 17:09:03 +0900

    5주차 퀴즈

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 # 유아로부터 구구단을 출력할 최대 단수를 입력받음
 max_dan = int(input("몇 단까지 출력할까요?: "))
 
-# 구구단 출력
-for dan in range(1, max_dan + 1):
-    print(f"--------{dan} ---------단")
-    for i in range(1, dan + 1):
-        result = dan * i
-        print(f"{dan} x {i} = {result}")
-    print()
+def biresult(dan, i):
+    result = dan * i
+
+ # 구구단 출력
+    for dan in range(1, max_dan + 1):
+        print(f"--------{dan} ---------단")
+        for i in range(1, dan + 1):
+            biresult = dan * i
+            print(dan,"X", i,"=", dan*i)
+        print()
+    return result
+
+biresult(max_dan, 1)
